dd887fed03b2c6cb83d3b88f2e8e94e6e6c97d1f
Dilmurod <dilmuroddelitot@gmail.com>
2024-07-31 18:09:00 +0500

    Order status selector

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,8 +1,10 @@
 import orders from "@/assets/data/order";
 import OrderItemListItem from "@/src/components/OrderItemListItem";
 import OrderListItem from "@/src/components/OrderListItem";
+import Colors from "@/src/constants/Colors";
+import { OrderStatusList } from "@/src/types";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 export default function OrderDetailsScreen() {
     const { id } = useLocalSearchParams()
@@ -21,6 +23,39 @@ export default function OrderDetailsScreen() {
                 renderItem={({ item }) => <OrderItemListItem item={item} />}
                 contentContainerStyle={{ gap: 10 }}
                 ListHeaderComponent={() => <OrderListItem order={order} />}
+                ListFooterComponent={() => (
+                    <>
+                      <Text style={{ fontWeight: 'bold' }}>Status</Text>
+                      <View style={{ flexDirection: 'row', gap: 5 }}>
+                        {OrderStatusList.map((status) => (
+                          <Pressable
+                            key={status}
+                            // onPress={() => updateStatus(status)}
+                            style={{
+                              borderColor: Colors.light.tint,
+                              borderWidth: 1,
+                              padding: 10,
+                              borderRadius: 5,
+                              marginVertical: 10,
+                              backgroundColor:
+                                order.status === status
+                                  ? Colors.light.tint
+                                  : 'transparent',
+                            }}
+                          >
+                            <Text
+                              style={{
+                                color:
+                                  order.status === status ? 'white' : Colors.light.tint,
+                              }}
+                            >
+                              {status}
+                            </Text>
+                          </Pressable>
+                        ))}
+                      </View>
+                    </>
+                  )}
             />
         </View>
     )
